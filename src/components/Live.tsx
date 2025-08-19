9911756ba3475d02a725328bb2986f582533656a
b-hojayev <bgnchojayew@gmail.com>
2025-08-19 10:14:52 +0500

    added live cursor

---
+++ src/components/Live.tsx
@@ -0,0 +1,43 @@
+import { useMyPresence, useOthers } from "@liveblocks/react";
+import LiveCursors from "./cursor/LiveCursors";
+import { useCallback } from "react";
+
+const Live = () => {
+  const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    // i am not sure do i need event.currentTarget.getBoundingClientRect().x this ???
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    updateMyPresence({ cursor: null, message: null });
+  }, []);
+
+  const handlePointerDown = useCallback((event: React.PointerEvent) => {
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  return (
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className="h-[100vh] w-full flex justify-center items-center text-center border-2 border-green-500"
+    >
+      <h1 className="text-2xl text-white">Liveblocks figma clone</h1>
+      <LiveCursors others={others} />
+    </div>
+  );
+};
+
+export default Live;
