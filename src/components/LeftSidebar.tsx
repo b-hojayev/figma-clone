fce0adb1b421132e0346e78116a6b31aa54f1b6a
b-hojayev <bgnchojayew@gmail.com>
2025-08-21 09:50:26 +0500

    added leftsidebar, rightsidebar layouts

---
+++ src/components/LeftSidebar.tsx
@@ -0,0 +1,9 @@
+const LeftSidebar = () => {
+  return (
+    <section className="flex flex-col border-t border-primary-gray-200 bg-primary-black text-gray-300 min-w-[227px] sticky left-0 h-full max-sm:hidden select-none overflow-y-auto pb-20">
+      <h3 className="px-5 pt-4 text-xs uppercase">Elements</h3>
+    </section>
+  );
+};
+
+export default LeftSidebar;
